540e530b022bf7e20168096fcb6138937b0e3bfb
SStepanova <sofia.stepanova@gmail.com>
2017-11-04 17:09:14 +0000

    #YAY, sonya's first ever git usage

---
+++ SelectResponder.java
@@ -0,0 +1,26 @@
+
+public class SelectResponder {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	private int serviceCategory(){
+		
+		
+		return 0;
+	}
+	
+	private int specialty(){
+		
+		return 0;
+		
+	}
+	
+	private int gender(){
+		return 0;
+		
+	}
+
+}
---
+++ User.java
@@ -0,0 +1,9 @@
+
+public class User {
+	String alias;
+	int age;
+	public enum Service {
+	    Army, Navy, RAF, Police, Fire, NHS, ServiceFamily, Other;
+	}
+	String location;
+}
