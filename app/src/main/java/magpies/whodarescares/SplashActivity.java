63253043fd7e8fab94adb6f13e22c95c727ca290
charlielockwood <charliejameslockwood@gmail.com>
2017-11-04 21:38:53 +0000

    Added splash page

---
+++ app/src/main/java/magpies/whodarescares/SplashActivity.java
@@ -0,0 +1,25 @@
+/**
+ * Created by charl on 04/11/2017.
+ */
+package magpies.whodarescares;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        try {
+            Thread.sleep(2250);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(intent);
+        finish();
+    }
+}
